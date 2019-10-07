fc56133328406c0204b333be2cd63ba56cbac84e
Nikita Biryukov <n.biryukov12@mail.ru>
2019-10-07 20:37:44 +0300

    fix

--- bday.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-a = input('Дата (дд-мм-гггг): ')
-a = a.split('.')
-aa = datetime.date(int(a[2]), int(a[1]), int(a[0]))
-bb = datetime.date.today()
-cc = (bb-aa) // 365
-print(str(cc).split()[0])
